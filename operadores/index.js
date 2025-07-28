eb8b824e00b9d17d9883ea06672f61c9b0653b10
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2025-07-28 18:08:33 -0300

    Introdução sobre operadores aritméticos, de atribuição e incremento

---
+++ operadores/index.js
@@ -0,0 +1,53 @@
+const num1 = 50;
+let num2 = 2;
+
+// Soma
+const soma = num1 + num2;
+
+console.log(soma);
+
+// Subtração
+const subtracao = num1 - num2;
+
+console.log(subtracao);
+
+// Divisão
+const divisao = num1 / num2;
+
+console.log(divisao);
+
+// Multiplicação
+const multiplicacao = num1 * num2;
+
+console.log(multiplicacao);
+
+// Potenciação
+const potenciacao = num1 ** num2;
+
+console.log(potenciacao);
+
+// Resto
+const resto = num1 % num2;
+
+console.log(resto);
+
+// Incrementos e Decrementos
+num2++;
+console.log(num2);
+
+++num2;
+console.log(num2);
+
+num2--;
+console.log(num2);
+
+num2 += 10;
+console.log(num2);
+
+num2 *= 10;
+console.log(num2);
+
+num2 **= 10;
+console.log(num2);
+
+// ...
