bc0c3739c9c134c6c525b570a26f6bb9deb5662f
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2025-07-28 18:07:32 -0300

    Introdução sobre variáveis e tipos primitivos

---
+++ variaveis/index.js
@@ -0,0 +1,18 @@
+// Variáveis se declara com "let"
+// Constantes se declara com "Const"
+
+// Tipos de dados primitivos:
+const num1 = 43; // Number
+let num2 = 32.321; // Number
+const string = 'Texto'; // String
+const bool = true; // Boolean
+let unfd; // Undefined
+// const undfd; // Isso dá erro
+const nu = null; // Null
+
+console.log(num1, typeof num1);
+console.log(num2, typeof num2);
+console.log(string, typeof string);
+console.log(bool, typeof bool);
+console.log(unfd, typeof unfd);
+console.log(nu, typeof nu);
